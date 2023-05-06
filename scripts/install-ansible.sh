763fa460460ccf193463bbd61013ace4aa111dd6
OsmOs <nicos2792@gmail.com>
2023-05-06 19:13:07 +0200

    add env

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-echo "Enter USER GITHUB: "
-read -p GITHUB_USER
-echo "Enter TOKEN GITHUB: "
-read -p GITHUB_TOKEN
+echo "Enter USER GITHUB"
+read -p 'User GITHUB | Appuyer sur [Enter]: ' GITHUB_USER </dev/tty
+echo "Enter TOKEN GITHUB"
+read -p 'TOKEN GITHUB | Appuyer sur [Enter]: ' GITHUB_TOKEN </dev/tty
 pip install ansible --user
 git config --global user.email "nicos2792@gmail.com"
 git config --global user.name "OsmOOs"
