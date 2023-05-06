4779b821f08a2d0af24ccbd16d87ebdc8823839c
OsmOOs <nicos2792@gmail.com>
2023-05-06 19:38:04 +0200

    Update install-ansible.sh

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-echo "Enter USER GITHUB"
-read -p 'User GITHUB | Appuyer sur [Enter]: ' GITHUB_USER </dev/tty
-echo "Enter TOKEN GITHUB"
-read -p 'TOKEN GITHUB | Appuyer sur [Enter]: ' GITHUB_TOKEN </dev/tty
+#echo "Enter USER GITHUB"
+#read -p 'User GITHUB | Appuyer sur [Enter]: ' GITHUB_USER </dev/tty
+#echo "Enter TOKEN GITHUB"
+#read -p 'TOKEN GITHUB | Appuyer sur [Enter]: ' GITHUB_TOKEN </dev/tty
 pip install ansible --user
 git config --global user.email "nicos2792@gmail.com"
 git config --global user.name "OsmOOs"
