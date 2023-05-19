1bdaed78812928b8f482486a009ca7db584f122a
OsmOs <nicos2792@gmail.com>
2023-05-19 16:20:29 +0200

    correctif name

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -7,5 +7,5 @@ pip install ansible --user
 git config --global user.email "nicos2792@gmail.com"
 git config --global user.name "OsmOOs"
 git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/OsmOOs/iac-vagrant.git
-/home/vagrant/.local/bin/ansible-playbook /home/vagrant/my-vagrant-box/provision.yml
+git clone https://github.com/OsmOOs/iac-vagrant.git
+/home/vagrant/.local/bin/ansible-playbook /home/vagrant/iac-vagrant/provision.yml
